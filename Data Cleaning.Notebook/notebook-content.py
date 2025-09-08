af0c40751b488684607456a80c7f62c98d6d6734
JosiahObo <joborekanhwo@goodwinlaw.com>
2025-09-08 11:18:34 -0400

    Committing 1 item from workspace aefa19bb-f970-428f-94bc-2908d00727c9

---
+++ Data Cleaning.Notebook/notebook-content.py
@@ -0,0 +1,43 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+# Data Cleaning Notebook
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+print('Hello World')
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+print('Data cleaned')
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
